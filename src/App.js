791bd04597af6e7de6ec22f68efcfb9b55e5a180
Aleksandra Sobaska <Aleksandra.Makowska@grupawp.pl>
2020-04-08 22:02:09 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  { id: 1, title: "Wiadomośc nr 1", body: "Zawartość wiadomości nr 1 ..." },
+  { id: 2, title: "Wiadomośc nr 2", body: "Zawartość wiadomości nr 2 ..." },
+];
+
+setInterval(() => {
+  const index = data.length + 1;
+  data.push({
+    id: index,
+    title: `Wiadomośc nr ${index}`,
+    body: `Zawartość wiadomości nr ${index} ...`,
+  });
+}, 5000);
+class App extends Component {
+  state = {
+    comments: [...data],
+  };
+
+  getData = () => {
+    if (this.state.comments.length !== data.length) {
+      console.log("aktualizacja");
+      this.setState({
+        comments: [...data],
+      });
+    } else {
+      console.log("dane takie same - nie aktualizujemy");
+    }
+  };
+
+  componentDidMount() {
+    this.idI = setInterval(this.getData, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.idI);
+  }
+  render() {
+    const comments = this.state.comments.map((comment) => (
+      <div key={comment.id}>
+        <h4>{comment.title}</h4>
+        <div>{comment.body}</div>
+      </div>
+    ));
+    // superowa rzecz :DDDDD reverse() odwraca nam kolejność wyświetlanych elementów
+    return <div className="App">{comments.reverse()}</div>;
+  }
 }
 
 export default App;
